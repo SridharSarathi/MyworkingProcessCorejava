81167d5ba353b056c60c5447e61494ae2e96bcdd
SridharSarathi <sridharrajavel1506@gmail.com>
2023-09-27 18:59:24 +0530

    changing execution

--- src/main/java/sarathi/App.java
+++ src/main/java/sarathi/App.java
@@ -1,4 +1,4 @@
-//package fundamentals;
+package sarathi;
 
 /**
  * Hello world!
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("this process will be executed in checking of git and github");
     }
 }
