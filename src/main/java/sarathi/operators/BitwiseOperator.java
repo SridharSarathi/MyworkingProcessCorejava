7e3a6c5c945276c9e2add9758383536fdac8b98d
SridharSarathi <sridharrajavel1506@gmail.com>
2023-10-28 21:17:15 +0530

    commit check

---
+++ src/main/java/sarathi/operators/BitwiseOperator.java
@@ -0,0 +1,19 @@
+package sarathi;
+
+public class BitwiseOperator
+ {
+  public static void main(String[] args) 
+  {
+    int num1=50;
+    int num2=20;
+    System.out.println(num1&num2);
+    System.out.println(num1|num2);
+    System.out.println(num1^num2);
+    System.out.println("My num1 is:"+num1+"\t my num2 is:"+num2);
+  }  
+}
+//          16  8  4  2  1
+/
+''
+//  a=10      0  1  0  1  0
+//  b=20      1  0  1  0  0
