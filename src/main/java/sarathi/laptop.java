df1d4070d6b76663c460cc0e90f7e74d0fefc02b
SridharSarathi <sridharrajavel1506@gmail.com>
2023-10-30 17:11:11 +0530

    control statement

--- src/main/java/sarathi/laptop.java
+++ src/main/java/sarathi/laptop.java
@@ -2,23 +2,43 @@ package sarathi;
 import java.util.Scanner;
 
 public class laptop 
-{
+ {
+   
    public static void main(String[] args)
     {
-    Scanner scan=new Scanner(System.in);
-    laptop sri=new laptop(System.in);
-    } 
-    public void entry()
-    {
-
+     laptop sri=new laptop();
+     Scanner scan=new Scanner(System.in);
+     System.out.println("------welcome to our laptop showroom------\n");
+     System.out.println("enter your amount");
+     int amount=scan.nextInt();
+     sri.entry(amount);
     }
 
-
-    
-
-
-
-
-
-
-}
+    public void entry(int value)
+    {
+      Scanner scan1=new Scanner(System.in);
+            if(value>=100000)
+            {
+                 System.out.println("You will buy appple");  
+            }
+                 if(value>=50000)
+                 {
+                  System.out.println("You will buy lenovo");
+                 }                            
+                 else if(value>=40000)
+                 {
+                  System.out.println("you will buy acer");
+                  }
+                 else if(value>=30000)
+                 {
+                   System.out.println("you will buy hp");
+                 } 
+                 else if(value>=20000)
+                 {
+                   System.out.println("you will buy HCL");
+                 } 
+            else{
+               System.out.println("currently not available");
+            }
+    }
+ }
